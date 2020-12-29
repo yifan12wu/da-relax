aeda2e5728c4732b095cd8d0c99d4c1329bbe45a
yifan12wu <yifan12.wu@gmail.com>
2020-12-29 03:58:28 -0500

    toy data

--- da_relax/data/toy.py
+++ da_relax/data/toy.py
@@ -1,75 +1,16 @@
 import numpy as np
 
-def sample_gs(mean, std, n, rand=None):
+from . import data as data_lib
+
+
+def sample_gaussian(mean, std, n, rand=None):
     if rand is None:
         rand = np.random
     x = rand.normal(size=[n, mean.shape[0]]) * std + mean
     return x
 
-DATA_0 = {
-    'source': {
-        'means': [[-1., 0.], [1., 0.]],
-        'stds': [[0.3, 0.3], [0.3, 0.3]],
-        'ns': [1, 1],
-        'labels': [0, 1],
-    },
-    'target': {
-        'means': [[-1., 0.], [1., 0.]],
-        'stds': [[0.3, 0.3], [0.3, 0.3]],
-        'ns': [2, 8],
-        'labels': [0, 1],
-    },
-}
 
-
-DATA_1 = {
-    'source': {
-        'means': [[-1., 0.], [1., 1.]],
-        'stds': [[0.3, 0.3], [0.3, 0.3]],
-        'ns': [1, 1],
-        'labels': [0, 1],
-    },
-    'target': {
-        'means': [[-1., 1.], [1., 0.]],
-        'stds': [[0.3, 0.3], [0.3, 0.3]],
-        'ns': [1, 1],
-        'labels': [0, 1],
-    },
-}
-
-
-DATA_2 = {
-    'source': {
-        'means': [[-1., 0.], [1., 1.]],
-        'stds': [[0.3, 0.3], [0.3, 0.3]],
-        'ns': [1, 1],
-        'labels': [0, 1],
-    },
-    'target': {
-        'means': [[-1., 1.], [1., 0.]],
-        'stds': [[0.3, 0.3], [0.3, 0.3]],
-        'ns': [2, 8],
-        'labels': [0, 1],
-    },
-}
-
-
-DATA_3 = {
-    'source': {
-        'means': [[-1., -0.3], [1., 0.3]],
-        'stds': [[0.1, 0.4], [0.1, 0.4]],
-        'ns': [5, 5],
-        'labels': [0, 1],
-    },
-    'target': {
-        'means': [[-0.3, -1.0], [0.3, 1.0]],
-        'stds': [[0.4, 0.1], [0.4, 0.1]],
-        'ns': [5, 5],
-        'labels': [0, 1],
-    },
-}
-
-DATA_4 = {
+DATA_CONFIG = {
     'source': {
         'means': [[-1., -0.3], [1., 0.3]],
         'stds': [[0.1, 0.4], [0.1, 0.4]],
@@ -84,7 +25,6 @@ DATA_4 = {
     },
 }
 
-DATA_LIST = [DATA_0, DATA_1, DATA_2, DATA_3, DATA_4]
 
 def generate_xy(config, n_samples, rand=None):
     xs = []
@@ -97,12 +37,16 @@ def generate_xy(config, n_samples, rand=None):
             config['stds'], 
             ns, 
             config['labels']):
-        x = sample_gs(np.array(mean), np.array(std), n, rand)
+        x = sample_gaussian(np.array(mean), np.array(std), n, rand)
         y = np.zeros(n, dtype=np.int32)
         y[:] = label
         xs.append(x)
         ys.append(y)
     return np.concatenate(xs, axis=0), np.concatenate(ys, axis=0)
+
+
+class Toy
+
 
 
 class MixGsDataset(object):
@@ -129,13 +73,6 @@ class MixGsDataset(object):
 
     def y(self):
         return self.y_
-
-
-def data_cfg_factory(data_id):
-    # data_id 'source(/target)_i'
-    s_t, i = data_id.split('_')
-    i = int(i)
-    return DATA_LIST[i][s_t]
 
 
 
