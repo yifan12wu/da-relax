1c2de371d64166885b3fda3b040f76bdac5b9622
yifan12wu <yifan12.wu@gmail.com>
2020-12-30 01:51:05 -0500

    learner, todo: model, optimizer

--- da_relax/train/da_learner.py
+++ da_relax/train/da_learner.py
@@ -7,13 +7,11 @@ import torch
 from torch import optim
 from torch.utils import tensorboard
 
-from ..data import datasets
-from ..networks import nets
-
 from ..tools import timer_tools
 from ..tools import py_tools
 from ..tools import flag_tools
 from ..tools import summary_tools
+from ..tools import torch_tools
 
 
 def get_optimizer(name):
@@ -33,49 +31,90 @@ def get_optimizer(name):
         raise ValueError('Unknown optimizer {}.'.format(name))
 
 
-class Trainer:
+class DALearner:
 
     @py_tools.store_args
     def __init__(self,
-            log_dir=os.path.join(os.getenv('HOME', '/'), 'tmp'),
+            # pytorch
+            device=None,
+            # data args
+            source_dataset=None,
+            target_dataset=None,
             # model training args
-            model=None,
-            dataset=None,
-            optimizer_config=None,
+            model_cfg=None,
+            optimizer_cfg=None,
             batch_size=128,
             # training loop args
+            log_dir=os.path.join(os.getenv('HOME', '/'), 'tmp/da-relax'),
             total_train_steps=30000,
             print_freq=2000,
             summary_freq=100,
             save_freq=10000,
-            test_freq=10000,
-            # others
-            device=None
+            eval_freq=10000,
             ):
         self._build()
-
-    @property
-    def model(self):
-        return self._model
 
     def _build(self):
         # device
         if self._device is None:
             self._device = torch.device(
                     'cuda' if torch.cuda.is_available() else 'cpu')
-            logging.info('Device: {}.'.format(self._device))
-        self._model.to(device=self._device)
+        logging.info('Device: {}.'.format(self._device))
+        self._build_model()
         self._build_optimizer()
+        self._build_dataset()
+        # information maintained during training and evaluation
         self._global_step = 0
         self._train_info = collections.OrderedDict()
+        self._eval_info = collections.OederedDict()
+        self._result_info = collections.OederedDict()
         self._summary_writer = tensorboard.SummaryWriter(
                 log_dir=self._log_dir)
+
+    def _build_model(self):
+        self._model.to(device=self._device)
 
     def _build_optimizer(self):
         cfg = self._optimizer_config
         opt_fn = get_optimizer(cfg.name)
         self._optimizer = opt_fn(self._model.parameters(),
                 lr=cfg.get_lr(0), weight_decay=cfg.get_wd(0))
+
+    def _build_dataset(self):
+        source_train = self._source_dataset.train
+        source_valid = self._source_dataset.valid
+        target_train = self._target_dataset.train
+        target_valid = self._target_dataset.valid
+        target_test = self._target_dataset.test
+        # setup data iteraters for training
+        self._source_train_iter = source_train.get_random_iterator(
+                self._batch_size)
+        self._target_train_iter = target_train.get_random_iterator(
+                self._batch_size)
+        self._source_valid_iter = source_valid.get_random_iterator(
+                self._batch_size)
+        self._target_valid_iter = target_valid.get_random_iterator(
+                self._batch_size)
+        def _get_eval_valid_iter():
+            return target_valid.get_one_shot_iterator(self._batch_size)
+        def _get_eval_test_iter():
+            return target_test.get_one_shot_iterator(self._batch_size)
+        self._get_eval_valid_iter = _get_eval_valid_iter
+        self._get_eval_test_iter = _get_eval_test_iter
+
+    def _save_model(self, step=None):
+        if step is not None:
+            filename = 'model-{}.ckpt'.format(step)
+        else:
+            filename = 'model.ckpt'
+        model_path = os.path.join(self._log_dir, filename)
+        torch.save(self._model.state_dict(), model_path)
+
+    def _get_current_lr(self):
+        return self._optimizer.param_groups[0]['lr']
+
+    def _get_current_wd(self):
+        return self._optimizer.param_groups[0]['weight_decay']
 
     def _maybe_update_optimizer(self):
         lr = self._optimizer_config.get_lr(self._global_step)
@@ -85,111 +124,105 @@ class Trainer:
         for param_group in self._optimizer.param_groups:
             param_group['weight_decay'] = wd
 
-    def _get_current_lr(self):
-        return self._optimizer.param_groups[0]['lr']
+    def _get_train_batch(self):
+        source_batch, _ = next(self._source_train_iter)
+        target_batch, _ = next(self._target_train_iter)
+        batch = [source_batch.x, source_batch.y, target_batch.x]
+        batch = [torch_tools.to_tensor(x, self._device) for x in batch]
+        return batch
 
-    def _get_current_wd(self):
-        return self._optimizer.param_groups[0]['weight_decay']
-
-    def _tensor(self, x):
-        if x.dtype in [np.float32, np.float64]:
-            return torch.tensor(x, dtype=torch.float32, device=self._device)
-        elif x.dtype in [np.int32, np.int64, np.uint8]:
-            return torch.tensor(x, dtype=torch.int64, device=self._device)
-        else:
-            raise ValueError('Unknown dtype {} for tensor.'
-                    .format(str(x.dtype)))
-
-    def _train_step(self, batch):
+    def _build_train_loss(self, batch):
         info = collections.OrderedDict()
-        x = self._tensor(batch.x)
-        y = self._tensor(batch.y)
         output_head = self._model(x, is_training=True)
         output_head.get_label(y)
-        self._optimizer.zero_grad()
-        output_head.loss.backward()
-        self._optimizer.step()
-        self._global_step += 1
-        self._maybe_update_optimizer()
         info['train_acc'] = output_head.acc.item()
         info['train_loss'] = output_head.loss.item()
+        return output_head.loss, info
+
+    def _train_step(self):
+        batch = self._get_train_batch()
+        loss, info = self._build_train_loss(batch)
+        self._optimizer.zero_grad()
+        loss.backward()
+        self._optimizer.step()
         info['lr'] = self._get_current_lr()
         info['wd'] = self._get_current_wd()
-        return info
+        self._train_info.update(info)
+        self._global_step += 1
+        self._maybe_update_optimizer()
+
+    def _get_valid_batch(self):
+        source_batch, _ = next(self._source_valid_iter)
+        target_batch, _ = next(self._target_valid_iter)
+        batch = [source_batch.x, source_batch.y, 
+                target_batch.x, target_batch.y]
+        batch = [torch_tools.to_tensor(x, self._device) for x in batch]
+        return batch
 
     def _get_valid_info(self, batch):
         info = collections.OrderedDict()
-        x = self._tensor(batch.x)
-        y = self._tensor(batch.y)
         output_head = self._model(x, is_training=False)
         output_head.get_label(y)
         info['valid_acc'] = output_head.acc.item()
         info['valid_loss'] = output_head.loss.item()
         return info
 
+    def _valid_step(self):
+        batch = self._get_valid_batch()
+        info = self._get_valid_info(batch)
+        self._train_info.update(info)
+
     def train(self):
-        dtr = self._dataset.train
-        dva = self._dataset.valid
-        train_iter = dtr.get_random_iterator(self._batch_size)
-        valid_iter = dva.get_random_iterator(self._batch_size)
-        # dte = self._dataset.test
-        global_timer = timer_tools.Timer()
-        local_timer = timer_tools.Timer()
-        final_eval_done = False
+        # 
+        timer = timer_tools.Timer()
         # -- start training
-        while self._global_step < self._total_train_steps:
-            train_batch, _ = next(train_iter)
-            # make sure that step is incr. by 1 in training_step
-            train_info = self._train_step(train_batch)
-            self._train_info.update(train_info)
+        for step in range(self._total_train_steps):
+            assert step == self._global_step
+            self._train_step()
+            ## 
             # write summary to tensorboard
-            if self._global_step % self._summary_freq == 0:
-                # get validation loss and acc
-                valid_batch, _ = next(valid_iter)
-                valid_info = self._get_valid_info(valid_batch)
-                self._train_info.update(valid_info)
+            if step == 0 or (step + 1) % self._summary_freq == 0:
+                # get validation info on a minibatch
+                self._valid_step()
+                steps_per_sec = timer.steps_per_sec(self._global_step)
+                self._train_info['steps_per_sec'] = steps_per_sec
                 summary_tools.write_summary(
                         writer=self._summary_writer,
                         info=self._train_info,
                         step=self._global_step)
             # print info
-            if self._global_step % self._print_freq == 0:
+            if step == 0 or (step + 1) % self._print_freq == 0:
                 summary_str = summary_tools.get_summary_str(
                         step=self._global_step,
                         info=self._train_info)
                 logging.info(summary_str)
-                steps_per_sec = local_timer.steps_per_sec_and_refresh(
-                        self._global_step)
-                logging.info('Steps per second: {:.4g}.'
-                        .format(steps_per_sec))
             # save ckpt
-            if self._global_step % self._save_freq == 0:
+            if (step + 1) % self._save_freq == 0:
                 self._save_model()
-            # run through validation set
-            if self._global_step % self._test_freq == 0:
-                logging.info('Testing at step {}/{}'.
+            # run through the whole validation set
+            if (step + 1) % self._eval_freq == 0:
+                logging.info('Evaluating on validation at step {}/{}.'.
                         format(self._global_step, self._total_train_steps))
-                valid_eval_iter = dva.get_one_shot_iterator(self._batch_size)
-                eval_info = self._evaluate(valid_eval_iter)
-                logging.info(summary_tools.get_summary_str(info=eval_info))
-                if self._global_step == self._total_train_steps:
-                    final_eval_done = True
-        # save ckpt
-        self._save_model()
-        if not final_eval_done:
-            # run through validation set
-            logging.info('Testing at step {}/{}'.
-                    format(self._global_step, self._total_train_steps))
-            valid_eval_iter = dva.get_one_shot_iterator(self._batch_size)
-            eval_info = self._evaluate(valid_eval_iter)
-            logging.info(summary_tools.get_summary_str(info=eval_info))
-        time_cost = global_timer.time_cost()
+                self._eval_on_valid()
+                # print and write summary for evaluation
+                logging.info(summary_tools.get_summary_str(
+                        info=self._eval_info))
+                summary_tools.write_summary(
+                        writer=self._summary_writer,
+                        info=self._eval_info,
+                        step=self._global_step)
+        # print out total training time
+        time_cost = timer.time_cost()
         logging.info('Training finished, time cost {:.4g}s.'
                 .format(time_cost))
+        # save ckpt
+        self._save_model()
+        # run through the test set
+        logging.info('Evaluating on test.')
+        self._eval_on_test()
+        logging.info(summary_tools.get_summary_str(info=self._result_info))
+        self._save_result()
 
-    def _save_model(self):
-        model_path = os.path.join(self._log_dir, 'model.ckpt')
-        torch.save(self._model.state_dict(), model_path)
 
     def _evaluate(self, data_iter):
         info = collections.OrderedDict()
@@ -206,9 +239,23 @@ class Trainer:
             total_loss += loss
             total_acc += acc
             total_size += size
-        info['acc'] = total_acc / total_size
-        info['loss'] = total_loss / total_size
+        info['eval_acc'] = total_acc / total_size
+        info['eval_loss'] = total_loss / total_size
         return info
+
+    def _eval_on_valid(self):
+        data_iter = self._get_eval_valid_iter()
+        self._eval_info.update(self._evaluate(data_iter))
+
+    def _eval_on_test(self):
+        data_iter = self._get_eval_test_iter()
+        self._result_info.update(self._evaluate(data_iter))
+
+    def _save_result(self):
+        result_file = os.path.join(self._log_dir, 'result.csv')
+        result = np.array(self._result_info['eval_acc'])
+        with open(result_file, 'w') as f:
+            np.savetxt(f, result, fmt='%.4g', delimiter=',')
 
 
 class TrainerConfig(flag_tools.ConfigBase):
